3c2af397b319c325720cb002f083f3ef3e3aa4e9
root <root@LAPTOP-EEV6NRCI>
2023-02-06 17:07:42 -0600

    step 1 register

--- models/user.js
+++ models/user.js
@@ -10,9 +10,23 @@ class User {
    *    {username, password, first_name, last_name, phone}
    */
 
-  static async register({username, password, first_name, last_name, phone}) { }
-
-  /** Authenticate: is this username/password valid? Returns boolean. */
+  static async register({username, password, first_name, last_name, phone}) {
+    let hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+    const result = await db.query(
+        `INSERT INTO users (
+              username,
+              password,
+              first_name,
+              last_name,
+              phone,
+              join_at,
+              last_login_at)
+            VALUES ($1, $2, $3, $4, $5, current_timestamp, current_timestamp)
+            RETURNING username, password, first_name, last_name, phone`,
+        [username, hashedPassword, first_name, last_name, phone]
+    );
+    return result.rows[0];
+  }
 
   static async authenticate(username, password) { }
 
